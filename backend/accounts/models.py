b8c3ea339f0b0c80871b4b6a49145709c5ab6dbe
gyeom <gyeom1205@naver.com>
2025-12-22 14:21:42 +0900

    Feat: MBTI 테스트 페이지 구현 및 회원가입 모델(User) 수정

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -5,8 +5,12 @@ class User(AbstractUser):
     nickname = models.CharField(max_length=20, null=True)
     # 추가 필드
     age = models.IntegerField(default=0, verbose_name="나이")
-    salary = models.IntegerField(default=0, verbose_name="연봉")
-    wealth = models.IntegerField(default=0, verbose_name="자산")
+    # ▼ 추가된 필드들
+    gender = models.CharField(max_length=10, null=True, blank=True)   # 남성/여성
+    job = models.CharField(max_length=20, null=True, blank=True)      # 학생/직장인...
+    income_source = models.CharField(max_length=20, null=True, blank=True) # 월급/용돈...
+    mbti = models.CharField(max_length=10, null=True, blank=True)
+    
     is_mydata_agreed = models.BooleanField(default=False, verbose_name="마이데이터 동의")
     # 찜한 상품 (M:N 관계)
     financial_products = models.TextField(blank=True, null=True)
