1a62f44d6941dead3c218f2e2d2c5eee6c8540bc
gyeom <gyeom1205@naver.com>
2025-12-23 18:00:01 +0900

    Feat: 챗봇 페이지 구현 및 MBTI 데이터 연동 로직 완성

--- backend/accounts/adapters.py
+++ backend/accounts/adapters.py
@@ -3,13 +3,15 @@ from allauth.account.adapter import DefaultAccountAdapter
 class CustomAccountAdapter(DefaultAccountAdapter):
     def save_user(self, request, user, form, commit=True):
         user = super().save_user(request, user, form, commit=False)
-        
-        # cleaned_data에서 가져와서 저장
         data = form.cleaned_data
+        
+        # ▼ 수정: salary, wealth 삭제 -> 새 필드 추가
         user.nickname = data.get('nickname')
         user.age = data.get('age')
-        user.salary = data.get('salary')
-        user.wealth = data.get('wealth')
+        user.gender = data.get('gender')
+        user.job = data.get('job')
+        user.income_source = data.get('income_source')
+        user.mbti = data.get('mbti')
         user.is_mydata_agreed = data.get('is_mydata_agreed')
         
         user.save()
